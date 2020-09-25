f25efdb89c94c07bc5d810f2d82821a2963f4d1f
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-25 05:25:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyCustomHolidayTo(holiday, name) {
   return (`Happy ${holiday}, ${name}!`)
 }
 
-function holidayCountdown(holiday, name) {
+function holidayCountdown(holiday, days) {
   return (`It's ${days} days until ${holiday}!`)
 }
